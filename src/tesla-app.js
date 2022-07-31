207ef41b270bdf58a9ee4d7f0032bafc07f1773d
Derek Smithson <derek.smithson@gpsinsight.com>
2022-07-31 12:05:21 -0700

    Added support for refresh tokens and package updates

--- src/tesla-app.js
+++ src/tesla-app.js
@@ -1,31 +1,33 @@
-const { delay } = require('bluebird');
-var tjs = require('teslajs');
-var path = require('path');
-var amqp = require('amqplib');
-var dotenv = require('dotenv');
-var process = require('process');
+
+const tjs = require('teslajs');
+const amqp = require('amqplib');
+const path = require('path');
+const dotenv = require('dotenv');
+const https = require('https');
+
+const timeout = ms => new Promise(res => setTimeout(res, ms))
 
 //Load environment variables from .env file
 dotenv.config({ path: path.join(process.env.ENV_FILES_DIR ? process.env.ENV_FILES_DIR : __dirname, '.env') });
 
-
 const verboseMode = process.env.VERBOSE_MODE;
-const refreshIntervalMs = 30000; 
+const refreshIntervalMs = 30000;
 const fullRefreshIterations = 4;
 
 let running = true;
 let isDriving = false;
 let currentIterationIndex = 0;
 
-function getSettings(callback) {
+async function getSettings() {
 
     let response = {
         username: process.env.TESLA_USERNAME,
         password: process.env.TESLA_PASSWORD,
-        token: process.env.TESLA_TOKEN
+        token: process.env.TESLA_TOKEN,
+        refreshToken: process.env.TESLA_REFRESH_TOKEN
     };
-    if((!response.username || !response.password) && !response.token) {
-        return callback(new Error("Need to provide envoronment variables.  Either TESLA_TOKEN OR TESLA_USERNAME and TESLA_PASSWORD"));
+    if((!response.username || !response.password) && !response.token && !response.refreshToken) {
+        return Promise.reject(new Error("Need to provide envoronment variables.  Either TESLA_TOKEN OR TESLA_REFRESH_TOKEN OR TESLA_USERNAME and TESLA_PASSWORD"));
     }
 
     let otherRequiredFields = 
@@ -43,12 +45,12 @@ function getSettings(callback) {
         let val = process.env[envName];
 
         if(!val) {
-            return callback(new Error(`Need to provide ${envName} environment variable`));
+            return Promise.reject(new Error(`Need to provide ${envName} environment variable`));
         }
         response[propertyName] = val;
     }
     
-    return callback(null, response);
+    return response;
 }
 
 function updateTeslaSensorData(settings, servicebus, callback) {
@@ -63,7 +65,7 @@ function updateTeslaSensorData(settings, servicebus, callback) {
     processSensors('vehicle_info', 'vehicle', options, fullObject, (err, vehicle) => {
         if (err || !vehicle) {
             console.error("Failed to get vehicle: " + err);
-            return callback(null);
+            return callback(err);
         }
 
         if(verboseMode) console.log(`Processed sensors: ${JSON.stringify(vehicle)}`);
@@ -141,7 +143,17 @@ function processSensors(sensorCategory, getSensorDataFuncName, options, fullObje
 
     //Get data from Tesla API
     tjs[getSensorDataFuncName](options, (err, results) => {
-        if (err) console.error(`Error getting data for ${sensorCategory}: ${err}`);
+        if (err) { 
+            console.error(`Error getting data for ${sensorCategory}: ${err}`);
+            console.error(JSON.stringify({
+                getSensorDataFuncName,
+                tjs,
+                sensorCategory,
+                options,
+                results
+            }));
+            return callback(err);
+        }
         if (results == null) return callback(null);
 
         if (verboseMode) console.log(`Received ${sensorCategory} state: ${JSON.stringify(results)}`);
@@ -193,34 +205,92 @@ function processList(items, doFunc, maxConcurrency, callback) {
     }
 }
 
-function loginToTesla(config, callback) {
+async function loginToTesla(config) {
 
-    //If token is supplied in our config, return it directly
-    if(config.token) {
-        return callback(null, config.token);
-    }
+    return await new Promise((resolve, reject) => {
 
-    tjs.login({
-        username: config.username,
-        password: config.password,
-        //mfaPassCode: mfaPassCode
-    }, function(err, result) {
-        if (result.error) {
-            console.log(JSON.stringify(result.error));
-            process.exit(1);
+        //If a refresh token is supplied in our config, get an access token from it
+        if(config.refreshToken) {
+            return getAccessTokenFromRefreshToken(config.refreshToken, (err, token) => {
+                config.token = token;
+                return resolve(token);
+            });
         }
 
-        var token = result.authToken;
-
-        if (token) {
-            console.log("Login Succesful!");
-            console.log("Token:  " + token);
+        //If token is supplied in our config, return it directly
+        else if(config.token) {
+            return resolve(config.token);
         }
 
-        //Save for future calls into this method during the current session
-        config.token = token;
-        return callback(null, token);
+        else {
+            tjs.login({
+                username: config.username,
+                password: config.password,
+                //mfaPassCode: mfaPassCode
+            }, function(err, result) {
+                if (result.error) {
+                    return reject("Login Error: " + JSON.stringify(result.error))
+                }
+
+                var token = result.authToken;
+
+                if (token) {
+                    console.log("Login Succesful!");
+                    console.log("Token:  " + token);
+                }
+
+                //Save for future calls into this method during the current session
+                config.token = token;
+                return resolve(token);
+            });
+        }
     });
+}
+
+function getAccessTokenFromRefreshToken(refreshToken, callback) {
+
+    const xFormBody = `${encodeURI('grant_type')}=${encodeURI('refresh_token')}&${encodeURI('client_id')}=${encodeURI("ownerapi")}&${encodeURI('refresh_token')}=${encodeURI(refreshToken)}`;
+    
+    var urlParams = {
+        host: 'auth.tesla.com', //No need to include 'http://' or 'www.'
+        port: 443,
+        path: '/oauth2/v3/token/',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Length': Buffer.byteLength(xFormBody)
+        }
+    };
+
+    performHttpPostAndGetResponse(urlParams, xFormBody, (err, response) => {
+        if(err) {
+            return callback(err);
+        }
+
+        let msg = JSON.parse(response);
+        console.log(`Received access token which will expire in ${msg.expires_in} seconds`);
+        return callback(null, msg.access_token);
+    });
+}
+
+function performHttpPostAndGetResponse(urlParams, body, callback) {
+    var str = '';
+    var cb = function(response) {
+        response.on('data', function (chunk) {
+            str += chunk;
+        });
+        response.on('end', function() {
+            callback(null, str);
+        });
+        response.on('error', function(err) {
+            console.warn("Error getting HTTP response on auth");
+            callback(err);
+        });
+    };
+
+    var req = https.request(urlParams, cb);
+    req.write(body);
+    req.end();    
 }
 
 function killProcess() {
@@ -237,47 +307,69 @@ process.on('uncaughtException', function(e) {
 async function run() {
 
     //Get settings
-    getSettings((err, settings) => {
-        if(err) {
-            console.log(err);
-            return;
-        }
-        loginToTesla(settings, async (err) => {
-            if(err) {
-                console.log("Failed to log in: " + err);
-                return;
-            }
-            
-            //Connect to service bus
-            let channel = null;
-            try {
-                //Connect to service bus
-                var conn = await amqp.connect(settings.amqpConnectionString);
-                channel = await conn.createChannel();
-                await channel.assertExchange(settings.exchangeName, settings.exchangeType, { durable: true });
-            }
-            catch(error) {
-                console.error("Failed to connect to AMQP:  " + error);
-                return;
-            }
+    let settings = null;
+    try {
+        console.log("Getting settings");
+        settings = await getSettings();   
+    } catch (error) {
+        console.error("Failed to get settings: " + error);
+        return;
+    }
 
-            //Register to run our update interval
-            while(running) {
-                //let pollingInterval = setInterval(updateTeslaSensorData, refreshIntervalMs, settings, channel, callback);
+    //Login to Tesla
+    let authToken = null;
+    try {
+        console.log("Getting Tesla auth token");
+        authToken = await loginToTesla(settings);
+        console.log("Logged in successfully");
+    } catch (error) {
+        console.error("Failed to log in: " + error);
+    }
+    
+    //Connect to Service Bus
+    let channel = null;
+    try {
+        console.log("Connecting to service bus");
+        var conn = await amqp.connect(settings.amqpConnectionString);
+        channel = await conn.createChannel();
+        await channel.assertExchange(settings.exchangeName, settings.exchangeType, { durable: true });
+    }
+    catch(error) {
+        console.error("Failed to connect to AMQP:  " + error);
+        return;
+    }
+
+    //Register to run our update interval
+    while(running) {
+        try {
+            await new Promise((resolve, reject) => {
                 updateTeslaSensorData(settings, channel, async (err) => {
                     if(err) {
-                        console.warn("Error running iteration: " + err);
-                        await delay(60000);
+                        return reject(err);
                     }
+                    resolve();                            
                 });
-                await delay(refreshIntervalMs);
+            });
+        } catch (err) {
+            if(err === "Unauthorized") {
+                //HACK:  Exit application, allowing it to be restarted (and reauthenticated)
+                console.warn("Last error indicated that we're no longer authenticated, so we'll retry auth");
+                try {
+                authToken = await loginToTesla(settings);
+                }
+                catch(authError) {
+                    console.error("Failed to reauthenticate: " + authError);
+                    process.exit(-2);
+                }
             }
-
-            // setTimeout(function() {
-            //     if (running) run();
-            // }, 10); 
-        });
-    });
+            else {
+                console.warn("Error running iteration (will wait and try again): " + err);
+            }
+        }
+        await timeout(60000);
+    }
 }
 
 run();
+
+console.log("Run loop exited");
